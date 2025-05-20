45ac84c394d6d1e2518598eee6c6a454e059e6cf
Guido Almeida <almeidaguidoe@gmail.com>
2025-05-20 10:45:00 -0300

    avancé con el index del buscador incluyendo los lugares, las radios y la galeria, falta videos y termino con eso

--- buscador.js
+++ buscador.js
@@ -81,7 +81,7 @@ const indexBuscador = [
     {
         url: "personajes.html#pj__el-nes-junior",
         titulo: "Personajes: El Nes Junior",
-        contenido: "el nes junior, nestor, pounes, pou nes"
+        contenido: "el nes junior, el ness junior, nestor, pouness, pou ness, jabubi jazz, junines, goyo 1"
     },
     {
         url: "personajes.html#pj__la-tana-lamorte",
@@ -381,7 +381,7 @@ const indexBuscador = [
     {
         url: "personajes.html#pj__galgadog",
         titulo: "Personajes: Galgadog",
-        contenido: "galgadog, galga, galgadogg"
+        contenido: "galgadog, galga, galgadogg, galga dog, galga dot"
     },
     {
         url: "personajes.html#pj__gonna-in-the-hal",
@@ -479,9 +479,331 @@ const indexBuscador = [
         contenido: "noelia pompa, noe pompa, enana noelia, haceme la co"
     },
     {
-        url: "personajes.html#pj__...",
-        titulo: "Personajes: ...",
-        contenido: "..."
+        url: "personajes.html#pj__oye-pablo",
+        titulo: "Personajes: Oye Pablo",
+        contenido: "oye pablo, pablo backyardigans"
+    },
+    {
+        url: "personajes.html#pj__pequeño-timmy",
+        titulo: "Personajes: Pequeño Timmy",
+        contenido: "pequeño timmy, little timmy, pequeño timmi, pequeño timi, pequeño timy"
+    },
+    {
+        url: "personajes.html#pj__rosalia",
+        titulo: "Personajes: Rosalia",
+        contenido: "la rosalia"
+    },
+    {
+        url: "personajes.html#pj__rowletnom",
+        titulo: "Personajes: Rowletnom",
+        contenido: "rowletnom, rouletnom"
+    },
+    {
+        url: "personajes.html#pj__scarlatta-singer",
+        titulo: "Personajes: Scarlatta Singer",
+        contenido: "scarlatta singer, scarlata singer, escarlata singer, escarlatta singer, scarlatta sings, scarlata sings, escarlata sings, escarlatta sings"
+    },
+    {
+        url: "personajes.html#pj__sebast912",
+        titulo: "Personajes: Sebast912",
+        contenido: "sebast912, sebas912, la coneja 912, coneja912"
+    },
+    {
+        url: "personajes.html#pj__storm-rita",
+        titulo: "Personajes: Storm Rita",
+        contenido: "storm rita, rita storm, rita tormenta"
+    },
+    {
+        url: "personajes.html#pj__tabla-pecosa",
+        titulo: "Personajes: Tabla Pecosa",
+        contenido: "tabla pecosa, tablita pecosa, cande pecosa"
+    },
+    {
+        url: "personajes.html#pj__la-tiendita-viajera",
+        titulo: "Personajes: La Tiendita Viajera",
+        contenido: "la tiendita viajera, little travelling store"
+    },
+    {
+        url: "personajes.html#pj__titi-fernandez",
+        titulo: "Personajes: Tití Fernández",
+        contenido: "titi fernandez, titi fernandes"
+    },
+    {
+        url: "personajes.html#pj__tortafrita",
+        titulo: "Personajes: Tortafrita",
+        contenido: "tortafrita, torta frita, tortafrita14"
+    },
+    {
+        url: "personajes.html#pj__torta-rita",
+        titulo: "Personajes: TortaRita",
+        contenido: "tortarita, torta rita, torta prostituta, torta puta"
+    },
+    {
+        url: "personajes.html#pj__virginia-lagos",
+        titulo: "Personajes: Virginia Lagos",
+        contenido: "virginia lagos, virginia dame la leche"
+    },
+    {
+        url: "personajes.html#pj__voglinn",
+        titulo: "Personajes: Voglinn",
+        contenido: "el king voglinn, el rey voglinn"
+    },
+    {
+        url: "personajes.html#pj__voglinn-goblin",
+        titulo: "Personajes: Voglinn Goblin",
+        contenido: "voglinn goblin, voglinn traficante, voglin goblin, voglin traficante, el king traficante, el king goblin"
+    },
+    {
+        url: "personajes.html#pj__voglinn-prostituta",
+        titulo: "Personajes: Voglinn Prostituta",
+        contenido: "voglinn prostituta, voglinn puta, voglin prostituta, voglin puta, voglinn mujer, voglin mujer"
+    },
+    {
+        url: "personajes.html#pj__lord-valdomero",
+        titulo: "Personajes: Lord Valdomero",
+        contenido: "lord valdomero, lord voldemort, voldi, voldy"
+    },
+    {
+        url: "personajes.html#pj__wazka-burbuja",
+        titulo: "Personajes: Wazka Burbuja",
+        contenido: "wazka burbuja, wasca burbuja"
+    },
+    
+
+    /* PÁGINA LUGARES*/
+    {
+        url: "lugares.html#lugares__san-fariseas",
+        titulo: "Lugares: San Fariseas",
+        contenido: "san fariseas, san andreas"
+    },
+    {
+        url: "lugares.html#lugares__coffe-town",
+        titulo: "Lugares: Coffe Town",
+        contenido: "coffe town, los santos"
+    },
+    {
+        url: "lugares.html#lugares__pizzeria-corazon-de-pizza",
+        titulo: "Lugares: Pizzería Corazón de Pizza",
+        contenido: "pizzeria corazon de pizza, pizza stack inc, locales de comida"
+    },
+    {
+        url: "lugares.html#lugares__mcpounes",
+        titulo: "Lugares: McPouNes",
+        contenido: "mcpouness, mc pou ness, mc pouness, macpouness, mac pouness, mac pou ness, el nes junior, el ness junior, nestor, pouness, pou ness, locales de comida"
+    },
+    {
+        url: "lugares.html#lugares__king-milapija",
+        titulo: "Lugares: King Milapija",
+        contenido: "king milapija, milanesas, michael jackson, pijas, locales de comida"
+    },
+    {
+        url: "lugares.html#lugares__corki-torts",
+        titulo: "Lugares: Corki Torts",
+        contenido: "corki torts, corky torts, tortafritas, torta fritas, locales de comida"
+    },
+    {
+        url: "lugares.html#lugares__kosiukaio",
+        titulo: "Lugares: Kosiukaio",
+        contenido: "kosiukaio, kosiucaio, cosiukaio, cosiucaio, caio gasolero, sexolero, locales de ropa, local de ropa"
+    },
+    {
+        url: "lugares.html#lugares__freddie-not-dead",
+        titulo: "Lugares: Freddie Not Dead",
+        contenido: "freddie not dead, freddy not dead, freddi not dead, fredie not dead, freddiereborn, freddie reborn, freddie rebourn, freddie rebourne, freddy reborn, freddy rebourn, freddy rebourne, locales de ropa, local de ropa"
+    },
+    {
+        url: "lugares.html#lugares__galga-rop",
+        titulo: "Lugares: Galga Rop",
+        contenido: "locales de ropa, local de ropa, galgarop galga rop galgarrop, galgadog, galga, galgadogg, galga dog, galga dot"
+    },
+    {
+        url: "lugares.html#lugares__desiderita",
+        titulo: "Lugares: DesideRita",
+        contenido: "locales de ropa, local de ropa, desiderita deside rita, desiderata, rita alejandra, la riri"
+    },
+    {
+        url: "lugares.html#lugares__monike",
+        titulo: "Lugares: MoNike",
+        contenido: "locales de ropa, local de ropa, monike, monique, monik, monobeatles"
+    },
+    {
+        url: "lugares.html#lugares__zonia",
+        titulo: "Lugares: Zonia",
+        contenido: "locales de ropa, local de ropa, zonia, la gata sonia, zara"
+    },
+    {
+        url: "lugares.html#lugares__ropa-otros",
+        titulo: "Lugares: Otros locales de ropa",
+        contenido: "locales de ropa, local de ropa, otros locales, king v, jouki time"
+    },
+    {
+        url: "lugares.html#lugares__casa-de-coffe",
+        titulo: "Lugares: Casa de Coffe",
+        contenido: "lugares varios, casa de coffe, hogar de coffe, calle comunidad"
+    },
+    {
+        url: "lugares.html#lugares__peluquerias",
+        titulo: "Lugares: Peluquerías Matt Vergona",
+        contenido: "lugares varios, peluquerias matt vergona, peluqueria matt vergona, peluquerias matt verona, peluqueria matt verona, cortes de pelo, corte de pelo"
+    },
+    {
+        url: "lugares.html#lugares__estudios-de-tatuajes",
+        titulo: "Lugares: Estudios de tatuajes",
+        contenido: "lugares varios, estudios de tatuajes, estudio de tatuajes, estudios de tattoos, estudio de tattoos, artisteando, anita artisteando, tatoos, tattoos"
+    },
+    {
+        url: "lugares.html#lugares__roscomercado",
+        titulo: "Lugares: RoscoMercado",
+        contenido: "lugares varios, supermercado, roscomercado, rosco mercado, super mercado"
+    },
+    {
+        url: "lugares.html#lugares__la-malla-de-ale",
+        titulo: "Lugares: Bar La Malla de Ale",
+        contenido: "lugares varios, bar, bar la malla de ale, ale escuchando"
+    },
+    {
+        url: "lugares.html#lugares__el-cibercafe",
+        titulo: "Lugares: El Cibercafé",
+        contenido: "lugares varios, el cibercafe, el ciber, el cyber"
+    },
+    {
+        url: "lugares.html#lugares__la-cueva-del-satiro",
+        titulo: "Lugares: La Cueva del Sátiro",
+        contenido: "lugares varios, la cueva del satiro, prostibulo, streapers, putas"
+    },
+    {
+        url: "lugares.html#lugares__el-gimnasio",
+        titulo: "Lugares: El Gimnasio",
+        contenido: "lugares varios, el gimnasio, el gym, boxeo"
+    },
+    {
+        url: "lugares.html#lugares__otros",
+        titulo: "Lugares: Otros Lugares",
+        contenido: "lugares varios, locales varios, telo ankara messi, morbo yanqui, ammunation, banco de coffe town, otros locales"
+    },
+
+    /* PÁGINA RADIOS*/
+    {
+        url: "radios.html#radio__proximo-diluvio",
+        titulo: "Radios: Próximo Diluvio",
+        contenido: "radio proximo diluvio, radio comunidad, pichi ciego, vispita, bloq mayuss"
+    },
+    {
+        url: "radios.html#radio__coffebeats",
+        titulo: "Radios: FM CoffeBeats",
+        contenido: "radio fm coffebeats, radio coffebeats, radio coffe beats, radio fm coffe beats, la rosalia"
+    },
+    {
+        url: "radios.html#radio__trolopop",
+        titulo: "Radios: TroloPop",
+        contenido: "radio trolopop, dj richard glam, radio trolos, radio gay"
+    },
+    {
+        url: "radios.html#radio__galactica",
+        titulo: "Radios: Galáctica",
+        contenido: "radio galactiaca, monguito, mi amigo mac, radio espacial, radio espacio, musica espacal, musica espacio, sovietwave, electronica"
+    },
+    {
+        url: "radios.html#radio__stroblin",
+        titulo: "Radios: Stroblin",
+        contenido: "radio stroblin, radio voglin, radio voglinn, radio voglinn stroblin, radio voglin stroblin, radio de voglinn"
+    },
+    {
+        url: "radios.html#radio__tropinelson",
+        titulo: "Radios: TropiNelson",
+        contenido: "radio tropinelson, radio cumbia, radio nelson, radio enano nelson, radio tropi nelson"
+    },
+    {
+        url: "radios.html#radio__tarantino",
+        titulo: "Radios: Tarantino",
+        contenido: "radio tarantino"
+    },
+    {
+        url: "radios.html#radio__jabrumix",
+        titulo: "Radios: JabruMix",
+        contenido: "radio jabrumix, radio la jabru, radio hip hop"
+    },
+    {
+        url: "radios.html#radio__redondos-ricoffe",
+        titulo: "Radios: Redondos de RiCoffe",
+        contenido: "radio redondos de ricoffe, radio redondos de ricota, radio los redondos de ricota, radio los redonditos de ricota, radio los redonditos de ricoffe, radio rdonditos de ricoffe, patricio rey, pr, radio ricotera"
+    },
+    {
+        url: "radios.html#radio-publicidades__inventadas",
+        titulo: "Radios: Publicidades Inventadas",
+        contenido: "publicidades radio, publicidades radiales, anuncios radiales, anuncios radio, alfajor lamorte, centro de ayuda ayudame hijo de puta, colchones garchobien, copymusic, haceme la co, el kiosco del nes junior, feo block, gran tateiro 2024, helados mister frio, infoflash, keporrito, king milapija, leche del bibel, mi familia es un enano, pintalabios el perro cachondo, trollassic park, trolasic park, trolassic park, trollaic park"
+    },
+    {
+        url: "radios.html#radio__publicidades__reales",
+        titulo: "Radios: Publicidades Reales",
+        contenido: "publicidades radio, publicidades radiales, anuncios radiales, anuncios radio, paletas tutsi pop sabor cajeta, consome panchi, pete, viernes de la jungla"
+    },
+
+    /* PÁGINA GALERIA DE IMAGENES*/
+    {
+        url: "galeria.html#galeria__historia",
+        titulo: "Galería: Historia del Juego",
+        contenido: "galeria historia del juego, misiones del juego, argumento del juego"
+    },
+    {
+        url: "galeria.html#galeria__personajes-principales",
+        titulo: "Galería: Personajes Principales",
+        contenido: "galeria personajes principales, personajes protagonistas, coffe tv, rita alejandra, baicito, tana lamorte, el nes junior"
+    },
+    {
+        url: "galeria.html#galeria__personajes-secundarios",
+        titulo: "Galería: Personajes Secundarios",
+        contenido: "galeria personajes secundarios, john frustrante, la jabru, edduerdo"
+    },
+    {
+        url: "galeria.html#galeria__bandas",
+        titulo: "Galería: Bandas",
+        contenido: "galeria bandas, banda comunidad, banda latam, banda mexicanos, banda ingleses brazucas, banda ingleses-brazucas, banda ingleses brasucas"
+    },
+    {
+        url: "galeria.html#galeria__fuerzas-seguridad",
+        titulo: "Galería: Fuerzas de Seguridad",
+        contenido: "galeria fuerzas de seguridad, policia twitch, swat paramount, ejercito youtube"
+    },
+    {
+        url: "galeria.html#galeria__peatones",
+        titulo: "Galería: Peatones",
+        contenido: "galeria peatones, galeria personajes"
+    },
+    {
+        url: "galeria.html#galeria__locales-de-comida",
+        titulo: "Galería: Locales de Comida",
+        contenido: "galeria locales de comida, local corazon de pizza, local king milapija, local mcpounes, local corki torts"
+    },
+    {
+        url: "galeria.html#galeria__locales-de-ropa",
+        titulo: "Galería: Locaes de Ropa",
+        contenido: "galeria locales de ropa, local kosiukaio, local freddie not dead, local galga rop, local desiderita, local monike, local zonia"
+    },
+    {
+        url: "galeria.html#galeria__otros-lugares",
+        titulo: "Galería: Otros Lugares",
+        contenido: "galeria otros lugares, galeria otros locales, casa de coffe, peluqueria matt vergona, estudio de tatuajes artisteando, tattoos, roscomercado, la malla de ale, el cibercafe, el cybercafe, la cueva del satiro, el gimnasio, el gym, king v, otros locales, locales varios"
+    },
+    {
+        url: "galeria.html#galeria__publicidades-productos",
+        titulo: "Galería: Publicidades y Productos",
+        contenido: "galeria publicidades y productos, galeria productos, cepichi, gran tateiro, jouki time, king v, carteles publicitarios, "
+    },
+    {
+        url: "galeria.html#galeria__imagenes-promocionales",
+        titulo: "Galería: Imágenes Promocionales",
+        contenido: "galeria imagenes promocionales, posters gta, galga dog"
+    },
+    {
+        url: "galeria.html#galeria__imagenes-de-pantalla-de-carga",
+        titulo: "Galería: Imágenes de Pantalla de Carga",
+        contenido: "galeria imagenes de pantalla de carga, imagenes pantalla carga, "
+    },
+    {
+        url: "galeria.html#galeria__fondos-de-menues",
+        titulo: "Galería: Fondos de Menúes",
+        contenido: "galeria fondos de menues, fondos de menus"
     },
 
 
